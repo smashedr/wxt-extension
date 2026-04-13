43cfa55c1497539a7291c1ba164428c69393df4f
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-04-12 23:59:11 -0700

    Updates (#16)

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { i18n } from '#imports'
+import { clickOpen } from '@/utils/extension.ts'
+
+// const config = useAppConfig()
+// console.log('config:', config)
+const manifest = chrome.runtime.getManifest()
+console.log('manifest:', manifest)
+</script>
+
+<template>
+  <div class="d-flex flex-row justify-content-center align-items-center">
+    <img src="@/assets/icon.svg" class="me-1" height="48" width="48" :alt="manifest.name" :title="manifest.name" />
+    <div>
+      <a
+        class="link-body-emphasis text-decoration-none fs-1"
+        :title="i18n.t('ui.text.homePage')"
+        :href="manifest.homepage_url"
+        target="_blank"
+        rel="nofollow"
+        @click.prevent="clickOpen"
+      >
+        {{ manifest.name }}</a
+      >
+      <a
+        class="link-body-emphasis text-decoration-none small"
+        :title="i18n.t('ui.text.releaseNotes')"
+        :href="`${manifest.homepage_url}/releases/tag/${manifest.version}`"
+        target="_blank"
+        rel="nofollow"
+        @click.prevent="clickOpen"
+      >
+        v<span class="version">{{ manifest.version }}</span></a
+      >
+    </div>
+  </div>
+</template>
--- src/entrypoints/options/App.vue
+++ src/entrypoints/options/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { i18n } from '#imports'
-import { clickOpen } from '@/utils/extension.ts'
 import { useTitle } from '@/composables/useTitle.ts'
 import { isFirefox } from '@/utils/system.ts'
 import BackToTop from '@/components/BackToTop.vue'
@@ -10,10 +9,9 @@ import OptionsForm from '@/components/OptionsForm.vue'
 import KeyboardShortcuts from '@/components/KeyboardShortcuts.vue'
 import PageFooter from '@/components/PageFooter.vue'
 import CopySupport from '@/components/CopySupport.vue'
+import PageHeader from '@/components/PageHeader.vue'
 
 console.debug('%c options/App.vue', 'color: Lime')
-
-const manifest = chrome.runtime.getManifest()
 
 useTitle(i18n.t('options.title'))
 </script>
@@ -22,38 +20,7 @@ useTitle(i18n.t('options.title'))
   <div class="d-flex align-items-center justify-content-center h-100 w-100 p-1 p-sm-3">
     <div class="m-auto pb-4 w-100">
       <div id="options-wrapper" class="glass-outline rounded rounded-3 w-100 m-auto p-2 p-sm-3">
-        <div class="d-flex flex-row justify-content-center align-items-center">
-          <img
-            src="@/assets/icon.svg"
-            class="me-1"
-            height="48"
-            width="48"
-            :alt="manifest.name"
-            :title="manifest.name"
-          />
-          <div>
-            <a
-              class="link-body-emphasis text-decoration-none fs-1"
-              :title="i18n.t('ui.text.homePage')"
-              :href="manifest.homepage_url"
-              target="_blank"
-              rel="nofollow"
-              @click.prevent="clickOpen"
-            >
-              {{ manifest.name }}</a
-            >
-            <a
-              class="link-body-emphasis text-decoration-none small"
-              :title="i18n.t('ui.text.releaseNotes')"
-              :href="`${manifest.homepage_url}/releases/tag/${manifest.version}`"
-              target="_blank"
-              rel="nofollow"
-              @click.prevent="clickOpen"
-            >
-              v<span class="version">{{ manifest.version }}</span></a
-            >
-          </div>
-        </div>
+        <PageHeader />
 
         <KeyboardShortcuts />
 
