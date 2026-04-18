07e4d199064209aaee14014edf33823a2c3c1161
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-18 10:33:39 -0700

    Update updateContextMenus

--- src/entrypoints/background/menus.ts
+++ src/entrypoints/background/menus.ts
@@ -19,11 +19,12 @@ export function updateContextMenus(enabled?: boolean) {
   console.debug('%cupdateContextMenus:', `color: ${enabled ? 'Lime' : 'Yellow'}`, enabled)
   if (!chrome.contextMenus) return console.debug('Skipping: chrome.contextMenus')
   chrome.contextMenus.removeAll().then(() => {
-    contexts.forEach((item) => {
-      // TODO: Do not mutate item.contexts in-place...
-      // if (!enabled && item.contexts?.includes('all')) item.contexts = ['action']
-      // console.log('item:', item)
-      chrome.contextMenus.create(item)
-    })
+    for (const [i, item] of contexts.entries()) {
+      // console.log(`item: ${i}`, item)
+      const entry = { ...item }
+      if (!enabled) entry.contexts = ['action']
+      console.log(`entry: ${i}`, entry.id, entry.contexts)
+      chrome.contextMenus.create(entry)
+    }
   })
 }
