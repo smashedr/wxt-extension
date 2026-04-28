76b91a3bfbff8bcd612b192bdce9c08ee29893aa
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-28 12:25:37 -0700

    Test

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -7,5 +7,16 @@ import { test, expect } from './fixtures.ts'
 
 test('popup page', async ({ page, extensionId }) => {
   await page.goto(`chrome-extension://${extensionId}/popup.html`)
+
   await expect(page.locator('.version')).toHaveText('0.0.0')
+
+  await page.locator('#bs-theme-dropdown').click()
+  await page.locator('button[data-bs-theme-value="dark"]').click()
+
+  await page.screenshot({ path: 'tests/screenshots/screenshot.png' })
+
+  // const locator = page.locator('#siteIcon')
+  // await expect(locator).toBeChecked()
+  // await locator.click()
+  // await expect(locator).not.toBeChecked()
 })
